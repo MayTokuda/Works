13d00016eb053fbfb94027ae56f28203d61bb109
MayTokuda <mei1984.05.18@gmail.com>
2023-03-27 20:58:22 +0900

    やり直し

---
+++ staff-Introduction/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link rel="stylesheet" href="style.css">
+        <title>テラノデザイン工房 | スタッフ紹介</title>
+    </head>
+
+    <body>
+        <!-- ヘッダー -->
+        <header class="header">
+            <div class="header__inner">
+                <div class="header__item">
+                    <h1 class="site-title"><a href="#"><img src="img/logo.png" alt=""></a></h1>
+                    <div class="header__link visible-pc"><a href="#">スタッフ紹介</a></div>
+                </div>
+
+                <!-- パンくず -->
+                <nav class="breadcrumb" aria-label="Breadcrumb">
+                    <ul class="breadcrumb__list">
+                        <li class="breadcrumb__item"><a href="#">HOME</a></li>
+                        <li class="breadcrumb__item"><span aria-current="location">スタッフ紹介</span></li>
+                    </ul>
+                </nav>
+
+            </div>
+        </header>
+
+        <main>
+            <!-- メインビジュアル -->
+            <img class="mv visible-pc" src="img/mv-pc.png" alt="">
+            <img class="mv visible-sp" src="img/mv-sp.png" alt="">
+
+            <div class="container">
+                <h2 class="page-title">スタッフ紹介</h2>
+                <p class="page-detail">概要やリード文が入ります概要やリード文が入ります概要やリード文が入ります。<br>
+                    概要やリード文が入ります概要やリード文が入ります概要やリード文が入ります。</p>
+                
+                    <!-- スタッフリスト -->
+                <ul class="staff__list">
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-1.png" alt="">
+                        <h3 class="staff__name staff__name__1">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-1.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-2.png" alt="">
+                        <h3 class="staff__name staff__name__2">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-2.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-3.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-3.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-4.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-4.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-5.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-5.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-6.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-6.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-7.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-7.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-8.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-8.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-9.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-9.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-10.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-10.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                    <li class="staff__item">
+                        <img class="staff__img" src="img/staff-11.png" alt="">
+                        <h3 class="staff__name">名前が入ります（職種が入ります）</h3>
+                        <time class="staff__date">2022年3月1日（火）</time>
+                        <p class="staff__detail">インタビューテキストインタビューテキストインタビューテキスト
+                            インタビューテキストインタビューテキストインタビューテキストインタビューテキスト</p>
+                        <a class="staff-detail__more-btn" href="php__detail/detail-11.php">続きを読む<span class="arrow-right icon"></span></a>
+                    </li>
+                </ul>
+
+                <!-- 一覧へのリンク -->
+                <a class="staff-list__more-btn"href="list.php">スタッフ一覧を見る<span class="arrow-right icon more-btn__icon"></span></a>
+
+            </div>
+        </main>
+
+        <!-- aside -->
+        <?php include "aside.php" ?>
+
+        <!-- フッター -->
+        <?php include "footer.php" ?>
+
+    </body>
+</html>
+        
